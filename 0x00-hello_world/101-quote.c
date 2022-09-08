f8bf145d3645786eb82ae38efc8449cfa345c1ff
DukeEthio <dukeethiopian@gmail.com>
2022-09-08 15:23:37 -0700

    c quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-
 /**
-* main - print atext
+* main - prints a text
 *
-* Return: always 1 (Success)
+* Return: Always 1 (Success)
 */
 int main(void)
 {
-write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
