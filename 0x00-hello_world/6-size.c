200509a755f744a0063d02f7034ecff0c71740e8
DukeEthio <dukeethiopian@gmail.com>
2022-09-08 13:24:16 -0700

    c size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 * main - print the string in the put function
 *
 * Description: using main function
-* this program prints "$size of data type"
+* this program prints "programming is like building amultilingual puzzle
 * Return: 0
 */
 int main(void)
@@ -12,13 +12,13 @@ int main(void)
 char c;
 int i;
 long li;
-long long lii;
+long long lli;
 float f;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
 printf("Size of an int: %ld byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-printf("Size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of a float int: %ld byte(s)\n", sizeof(f));
 return (0);
 }
