c0352cded320e0353528d832e29f29088efdafb1
DukeEthio <dukeethiopian@gmail.com>
2022-09-09 16:03:22 -0700

    c 0-positive-negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,67 +1,33 @@
-#include <stdlib.h>                                                                                         
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-#include <time.h>                                                                                           
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+int n;
 
-include <stdio.h>                                                                                           
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-                                                                                                            
+if (n == 0)
+{
+printf("%i is zero\n", n);
+}
 
-/**                                                                                                         
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
 
-* main - Entry point                                                                                        
+else
+{
+printf("%i is positive\n", n);
+}
 
-*                                                                                                           
-
-* Return: Always 0 (Success/correct)                                                                        
-
-*/                                                                                                          
-
-                                                                                                            
-
-int main(void)                                                                                              
-
-{                                                                                                           
-
-int n;                                                                                                      
-
-                                                                                                            
-
-srand(time(0));                                                                                             
-
-n = rand() - RAND_MAX / 2;                                                                                  
-
-                                                                                                            
-
-if (n == 0)                                                                                                 
-
-{                                                                                                           
-
-printf("%i is zero\n", n);                                                                                  
-
-}                                                                                                           
-
-                                                                                                            
-
-else if (n < 0)                                                                                             
-
-{                                                                                                           
-
-printf("%i is negative\n", n);                                                                              
-
-}                                                                                                           
-
-                                                                                                            
-
-else                                                                                                        
-
-{                                                                                                           
-
-printf("%i is positive\n", n);                                                                              
-
-}                                                                                                           
-
-                                                                                                            
-
-return (0);                                                                                                 
-
+return (0);
 }
