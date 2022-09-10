f49881ecc5ebba1d960d20f5d97a000dcad2749c
DukeEthio <dukeethiopian@gmail.com>
2022-09-09 17:03:07 -0700

    c 100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,28 +3,30 @@
 /**
 * main - entry point
 *
-* codes for prting all possible combination of d/t digits
-* in assending order and separeted by comma followed by space
+* codes for printing all possible combination of two d/t digits
+* in assending order and separated by comma followed by space.
 *
-* Return: 0
+* Return: 0 Success
 */
 int main(void)
 {
 int digit1, digit2;
 
-for (digit1 = 0; digit1 < 9; digit1++)
+for (digit1 = 0; diigit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar ((digit1 % 10) + '0');
-putchar ((digit2 % 10) + '0');
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
 if (digit1 == 8 && digit2 == 9)
 continue;
 
-putchar (',');
-putchar (' ');
+putchar(',');
+putchar(' ');
 }
 }
-putchar ('\n');
+putchar('\n');
+
+return (0);
 }
