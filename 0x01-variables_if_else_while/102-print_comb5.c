d050b5e430df24a252192a857b92221bafa72cec
DukeEthio <dukeethiopian@gmail.com>
2022-09-10 04:37:05 -0700

    c 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
 * main - entry point
 *
-* code prints two digit combination
+* codes print two combination digit
 * Return: 0 Success
 */
 int main(void)
 {
 int n1 = 0, n2;
-while(n1 <= 99)
+while (n1 <= 99)
 {
 n2 = n1;
-while(n2 <= 99)
+while (n2 <= 99)
 {
 if (n2 != n1)
 {
@@ -23,7 +22,6 @@ putchar(' ');
 putchar((n2 / 10) + 48);
 putchar((n2 % 10) + 48);
 if (n1 != 98 || n2 != 99)
-{
 putchar(',');
 putchar(' ');
 }
