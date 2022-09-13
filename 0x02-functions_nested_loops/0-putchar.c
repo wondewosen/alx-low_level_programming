ba0ff59590f594229fb4d74de5d1532be67a35da
DukeEthio <dukeethiopian@gmail.com>
2022-09-13 07:39:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
-#include <"_putchar.h"
+#include "main.h"
 /**
-* main - function
-*
-* Return: 0
+* main print putchar
+* Return: always 0
 */
 int main(void)
 {
-char _putchar[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-unsigned int c;
-for (c = 0; c < sizeof(_putchar); c++)
-{
-_putchar(_putchar[c]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
